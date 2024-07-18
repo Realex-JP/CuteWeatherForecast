86dfb71809535b3aafc34b10bc76f8c81be51c59
Realex-JP <154582315+Realex-JP@users.noreply.github.com>
2024-07-18 23:48:52 +0900

    Add files via upload

---
+++ CuteWeatherForecast/cwf/src/main/java/com/example/Config.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.awt.*;
+import javax.imageio.*;
+import javax.swing.*;
+
+public final class Config {
+    final static int updateTime = 3600000; // 更新する頻度（1時間）
+
+    final static Color themeColor = new Color(0xC8C4FF);
+
+    final static Dimension dim = new Dimension(600, 400);
+
+    final static Font mainFont = new Font("Agency FB", Font.PLAIN, 70);
+    final static Font subFont = new Font("Bauhaus 93", Font.PLAIN, 20);
+
+    final static ImageIcon sunny = new ImageIcon(Config.class.getResource("/sunny_2570483.png"));
+    final static ImageIcon cloudy = new ImageIcon(Config.class.getResource("/cloud_2570486.png"));
+    final static ImageIcon rainy = new ImageIcon(Config.class.getResource("/rain_2570494.png"));
+    final static ImageIcon snowy = new ImageIcon(Config.class.getResource("/snow_2570507.png"));
+
+    public static Image sunnyBg;
+    public static Image cloudyBg;
+    public static Image rainyBg;
+    public static Image snowyBg;
+
+    static {
+        try {
+            sunnyBg = ImageIO.read(Config.class.getResource("/sunnyBg.jpg"));
+            cloudyBg = ImageIO.read(Config.class.getResource("/cloudyBg.jpg"));
+            rainyBg = ImageIO.read(Config.class.getResource("/rainyBg.jpg"));
+            snowyBg = ImageIO.read(Config.class.getResource("/snowyBg.jpg"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ CuteWeatherForecast/cwf/src/main/java/com/example/CuteWeatherForecast.java
@@ -0,0 +1,207 @@
+package com.example;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.swing.*;
+import org.json.JSONObject;
+
+import static com.example.Config.*;
+
+public class CuteWeatherForecast extends JPanel implements Runnable {
+
+    Thread fThread;
+    private JLabel temperatureLabel;
+    private JLabel humidityLabel;
+    private JLabel weatherLabel;
+
+    private JButton setTokyo;
+    private JButton setParis;
+    private JButton setLondon;
+
+    private Image bgImg;
+
+    public CuteWeatherForecast() {
+        setLayout(new BorderLayout());
+
+        JPanel weatherInfoPanel = new JPanel(new GridLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (bgImg != null) {
+                    g.drawImage(bgImg, 0, 0, getWidth(), getHeight(), this);
+                }
+            }
+        };
+
+        temperatureLabel = new JLabel();
+        temperatureLabel.setHorizontalAlignment(JLabel.CENTER);
+        temperatureLabel.setFont(mainFont);
+        weatherLabel = new JLabel();
+        weatherLabel.setHorizontalAlignment(JLabel.CENTER);
+        humidityLabel = new JLabel();
+        humidityLabel.setHorizontalAlignment(JLabel.CENTER);
+        humidityLabel.setFont(mainFont);
+
+        weatherInfoPanel.add(temperatureLabel);
+        weatherInfoPanel.add(weatherLabel);
+        weatherInfoPanel.add(humidityLabel);
+
+        weatherInfoPanel.setBackground(themeColor);
+        add(weatherInfoPanel, BorderLayout.CENTER);
+
+        setTokyo = new JButton("Tokyo");
+        setTokyo.setFont(subFont);
+        setTokyo.setBorderPainted(false);
+        setTokyo.setContentAreaFilled(false);
+
+        setParis = new JButton("Paris");
+        setParis.setFont(subFont);
+        setParis.setBorderPainted(false);
+        setParis.setContentAreaFilled(false);
+
+        setLondon = new JButton("London");
+        setLondon.setFont(subFont);
+        setLondon.setBorderPainted(false);
+        setLondon.setContentAreaFilled(false);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 5));
+        buttonPanel.setBackground(themeColor);
+
+        buttonPanel.add(setTokyo);
+        buttonPanel.add(setParis);
+        buttonPanel.add(setLondon);
+
+        add(buttonPanel, BorderLayout.NORTH);
+
+        setPreferredSize(dim);
+
+        setTokyo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateWeatherInfo("Tokyo");
+            }
+        });
+
+        setParis.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateWeatherInfo("Paris");
+            }
+        });
+
+        setLondon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateWeatherInfo("London");
+            }
+        });
+        startThread();
+    }
+
+    private void startThread() {
+        if (fThread == null) {
+            fThread = new Thread(this);
+            fThread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (Thread.currentThread() == fThread) {
+            updateWeatherInfo("Tokyo");
+            try {
+                Thread.sleep(updateTime);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    private void updateWeatherInfo(String city) {
+        try {
+            String apiKey = "83719ed25335223ceba0fb2b680d4076";
+            String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&appid=" + apiKey;
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            connect.setRequestMethod("GET");
+
+            int responseCode = connect.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+
+                JSONObject jsonResponse = new JSONObject(content.toString());
+                String temperature = jsonResponse.getJSONObject("main").getInt("temp") + "°C";
+                String humidity = jsonResponse.getJSONObject("main").getInt("humidity") + "%";
+                String weather = jsonResponse.getJSONArray("weather").getJSONObject(0).getString("main");
+
+                temperatureLabel.setText(temperature);
+                humidityLabel.setText(humidity);
+
+                ImageIcon icon = null;
+                switch (weather) {
+                    case "Clear":
+                        icon = sunny;
+                        bgImg = sunnyBg;
+                        break;
+                    case "Clouds":
+                        icon = cloudy;
+                        bgImg = cloudyBg;
+                        break;
+                    case "Rain":
+                        icon = rainy;
+                        bgImg = rainyBg;
+                        break;
+                    case "Snow":
+                        icon = snowy;
+                        bgImg = snowyBg;
+                        break;
+                }
+                if (icon != null) {
+                    weatherLabel.setIcon(icon);
+                } else {
+                    System.out.println("No icon found for weather: " + weather);
+                }
+
+                repaint();
+            
+            } else {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (bgImg != null) {
+            g.drawImage(bgImg, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setTitle("CuteWeatherForecast");
+            f.setResizable(false);
+            f.add(new CuteWeatherForecast(), BorderLayout.CENTER);
+            f.pack();
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        });
+    }
+}
