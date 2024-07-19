a83567b5292711068e15eaf073de0a51d40d870f
Realex <1cpb3208@mail.u-tokai.ac.jp>
2024-07-19 16:22:13 +0900

    update

--- CuteWeatherForecast/cwf/src/main/java/com/example/Config.java
+++ CuteWeatherForecast/cwf/src/main/java/com/example/Config.java
@@ -15,6 +15,31 @@ public final class Config {
     final static Font subFont = new Font("Bauhaus 93", Font.PLAIN, 20);
     final static Font locationFont = new Font("Bauhaus 93", Font.PLAIN, 40);
 
+    final static String location1 = "Tokyo";
+    final static String location2 = "Paris";
+    final static String location3 = "London";
+
+    final static JButton setLocation1 = new JButton("Tokyo");
+    final static JButton setLocation2 = new JButton("Paris");
+    final static JButton setLocation3 = new JButton("London");
+    final static JButton setOther = new JButton("Other");
+
+    static {
+        setLocation1.setFont(subFont);
+        setLocation1.setBorderPainted(false);
+        setLocation1.setContentAreaFilled(false);
+        setLocation2.setFont(subFont);
+        setLocation2.setBorderPainted(false);
+        setLocation2.setContentAreaFilled(false);
+        setLocation3.setFont(subFont);
+        setLocation3.setBorderPainted(false);
+        setLocation3.setContentAreaFilled(false);
+        setOther.setFont(subFont);
+        setLocation3.setBorderPainted(false);
+        setLocation3.setContentAreaFilled(false);
+    }
+
+
     final static ImageIcon sunny = new ImageIcon(Config.class.getResource("/sunny_2570483.png"));
     final static ImageIcon cloudy = new ImageIcon(Config.class.getResource("/cloud_2570486.png"));
     final static ImageIcon rainy = new ImageIcon(Config.class.getResource("/rain_2570494.png"));
